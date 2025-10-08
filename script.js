06351e91ccbdafa0acd60be7ea11f4338ee39cc2
Cat-haha <28zgreene@centralcatholic.org>
2025-10-08 16:40:23 +0000

    added some features i think

--- script.js
+++ script.js
@@ -9,3 +9,23 @@
           main.style.marginLeft = "160px";
         }
       }
+
+    const link = document.getElementById('kanyeText');
+    const image = document.getElementById('kanyeStaring');
+
+    link.addEventListener('mouseenter', () => {
+      image.style.display = 'block';
+    });
+
+    link.addEventListener('mouseleave', () => {
+      image.style.display = 'none';
+    });
+
+function confirmLeave(url) {
+  const leave = confirm("Are you sure you want to leave this page? You will be redirected to: " + url);
+  if (leave) {
+    window.location.href = url;
+  } else {
+
+  }
+}
